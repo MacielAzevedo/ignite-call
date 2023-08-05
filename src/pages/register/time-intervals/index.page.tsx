2abbc69eab51780c8be8e129bbab390461e003b5
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-08-05 18:44:26 -0300

    feat: Configuring availability - Availability form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -15,8 +15,79 @@ import {
   IntervalItem,
 } from './styles';
 import { ArrowRight } from 'phosphor-react';
+import { z } from 'zod';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { getWeekDays } from '@/utils/get-week-days';
+
+const timeIntervalFormSchema = z.object({});
+
+type TimeIntervalFormData = z.infer<typeof timeIntervalFormSchema>;
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        {
+          weekDay: 0,
+          enabled: false,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 1,
+          enabled: true,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 2,
+          enabled: true,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 3,
+          enabled: true,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 4,
+          enabled: true,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 5,
+          enabled: true,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+        {
+          weekDay: 6,
+          enabled: false,
+          startTime: '08:00',
+          endTime: '18:00',
+        },
+      ],
+    },
+  });
+
+  const weekDays = getWeekDays();
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  });
+
+  async function handleSetTimeIntervals() {}
+
   return (
     <Container>
       <Header>
@@ -29,19 +100,33 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Checkbox />
+                  <Text>{weekDays[field.weekDay]}</Text>
+                </IntervalDay>
 
-            <IntervalInputs>
-              <TextInput size="md" type="time" step={60} />
-              <TextInput size="md" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+                <IntervalInputs>
+                  <TextInput
+                    size="md"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size="md"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            );
+          })}
         </IntervalContainer>
 
         <Button type="submit">
